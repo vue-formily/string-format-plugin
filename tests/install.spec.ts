e88aa88d871baf91c34eedbd0b92cf9e0033b27e
an ha <haan.an@yahoo.com.vn>
2021-09-04 17:52:04 +0700

    chore: update release note scripts

--- tests/install.spec.ts
+++ tests/install.spec.ts
@@ -2,7 +2,7 @@ import { createLocalVue, mount } from '@vue/test-utils';
 import VueFormily from '@vue-formily/formily';
 import stringFormat from '@/.';
 
-describe('installation', () => {
+describe('Installation', () => {
   let localVue: any;
 
   beforeEach(() => {
